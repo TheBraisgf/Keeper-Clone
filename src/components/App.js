e49622c6d05ceeec025c5f823aa9b6f58a2a583e
¨braisgf¨ <braisgf@gmail.com>
2022-11-10 18:39:47 +0100

    Make changes to new deploy

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,16 @@
-import React from 'react';
-import Header from './header';
-import Footer from './footer';
-import Note from './note';
+import React from "react";
+import Header from "./header.js";
+import Footer from "./footer.js";
+import Note from "./note.js";
 
 function App() {
-  return <div>
-    <Header />
-    <Note />
-    <Footer />
-  </div>
-};
+  return (
+    <div>
+      <Header />
+      <Note />
+      <Footer />
+    </div>
+  );
+}
 
 export default App;
